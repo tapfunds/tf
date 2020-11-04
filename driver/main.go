d4e360a737989edbb7f2c201fd982350c6ef87d1
Qwelian Tanner <qweliantanner@gmail.com@users.noreply.github.com>
2020-11-03 20:44:27 -0500

    api up: need to figure out how to access tables

--- driver/main.go
+++ driver/main.go
@@ -6,18 +6,18 @@ import (
 	"tfdb/models"
 	"tfdb/routes"
 	"gorm.io/gorm"
+	"gorm.io/driver/postgres"
 )
 
 var err error
 
 func main() {
-	// dsn := "user=postgres password=*Grow dbname=tapfunds port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	dsn := "user=postgres password=*Grow dbname=tapfunds port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	// db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	config.DB, err = gorm.Open(postgres.Open(config.DbURL(config.BuildDBConfig())), &gorm.Config{})
+	config.DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println("Status:", err)
 	}
-	defer config.DB.Close()
 	config.DB.AutoMigrate(&models.PlaidIntegration{})
 	r := routes.SetupRouter()
 	//runnin
