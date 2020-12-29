4e560839b16855a1d695e8b32e6030200f59f923
Qwelian D. Tanner <qweliantanner@gmail.com>
2020-12-29 15:32:36 -0500

    token controller done

--- driver/controllers/token_controller.go
+++ driver/controllers/token_controller.go
@@ -1,13 +1,18 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strconv"
+	"tfdb/auth"
 	"tfdb/models"
+	"tfdb/utils/formaterror"
+
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
 )
 
-// POST /token
 // Create new token
 func (server *Server) CreatePlaidInfo(c *gin.Context) {
 	//clear previous error if any
@@ -102,26 +107,91 @@ func (server *Server) GetUserIntegration(c *gin.Context) {
 	})
 }
 
-// DELETE /token/:id
-// Delete a token
-func DeletePlaidInfo(c *gin.Context) {
+func (server *Server) UpdateIntegration(c *gin.Context) {
+
 	//clear previous error if any
 	errList = map[string]string{}
 
-	db := c.MustGet("db").(*gorm.DB)
-
-	// Get model if exist
-	var info models.PlaidIntegration
-	if err := db.Where("id = ?", c.Param("id")).First(&info).Error; err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+	postID := c.Param("id")
+	// Check if the post id is valid
+	pid, err := strconv.ParseUint(postID, 10, 64)
+	if err != nil {
+		errList["Invalid_request"] = "Invalid Request"
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": http.StatusBadRequest,
+			"error":  errList,
+		})
 		return
 	}
+	//CHeck if the auth token is valid and  get the user id from it
+	uid, err := auth.ExtractTokenID(c.Request)
+	if err != nil {
+		errList["Unauthorized"] = "Unauthorized"
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"status": http.StatusUnauthorized,
+			"error":  errList,
+		})
+		return
+	}
+	//Check if the post exist
+	integration := models.PlaidIntegration{}
+	err = server.DB.Debug().Model(models.PlaidIntegration{}).Where("id = ?", pid).Take(&integration).Error
+	if err != nil {
+		errList["No_post"] = "No Post Found"
+		c.JSON(http.StatusNotFound, gin.H{
+			"status": http.StatusNotFound,
+			"error":  errList,
+		})
+		return
+	}
+	if uid != integration.UserID {
+		errList["Unauthorized"] = "Unauthorized"
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"status": http.StatusUnauthorized,
+			"error":  errList,
+		})
+		return
+	}
+	// Read the data posted
+	body, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		errList["Invalid_body"] = "Unable to get request"
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"status": http.StatusUnprocessableEntity,
+			"error":  errList,
+		})
+		return
+	}
+	// Start processing the request data
+	editedIntegration := models.PlaidIntegration{}
+	err = json.Unmarshal(body, &post)
+	if err != nil {
+		errList["Unmarshal_error"] = "Cannot unmarshal body"
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"status": http.StatusUnprocessableEntity,
+			"error":  errList,
+		})
+		return
+	}
+	editedIntegration.ID = origPost.ID //this is important to tell the model the post id to update, the other update field are set above
+	editedIntegration.UserID = origPost.UserID
 
-	db.Delete(&info)
-
-	c.JSON(http.StatusOK, gin.H{"data": true})
+	integrationtUpdated, err := editedIntegration.UpdateAPost(server.DB)
+	if err != nil {
+		errList := formaterror.FormatError(err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status": http.StatusInternalServerError,
+			"error":  errList,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"status":   http.StatusOK,
+		"response": integrationtUpdated,
+	})
 }
-func (server *Server) DeletePost(c *gin.Context) {
+
+func (server *Server) DeleteIntegration(c *gin.Context) {
 
 	postID := c.Param("id")
 	// Is a valid post id given to us?
@@ -135,7 +205,7 @@ func (server *Server) DeletePost(c *gin.Context) {
 		return
 	}
 
-	fmt.Println("this is delete post sir")
+	fmt.Println("We will take everything..")
 
 	// Is this user authenticated?
 	uid, err := auth.ExtractTokenID(c.Request)
@@ -148,8 +218,8 @@ func (server *Server) DeletePost(c *gin.Context) {
 		return
 	}
 	// Check if the post exist
-	post := models.Post{}
-	err = server.DB.Debug().Model(models.Post{}).Where("id = ?", pid).Take(&post).Error
+	integration := models.PlaidIntegration{}
+	err = server.DB.Debug().Model(models.PlaidIntegration{}).Where("id = ?", pid).Take(&integration).Error
 	if err != nil {
 		errList["No_post"] = "No Post Found"
 		c.JSON(http.StatusNotFound, gin.H{
@@ -159,7 +229,7 @@ func (server *Server) DeletePost(c *gin.Context) {
 		return
 	}
 	// Is the authenticated user, the owner of this post?
-	if uid != post.AuthorID {
+	if uid != integration.UserID {
 		errList["Unauthorized"] = "Unauthorized"
 		c.JSON(http.StatusUnauthorized, gin.H{
 			"status": http.StatusUnauthorized,
@@ -168,29 +238,7 @@ func (server *Server) DeletePost(c *gin.Context) {
 		return
 	}
 	// If all the conditions are met, delete the post
-	_, err = post.DeleteAPost(server.DB)
-	if err != nil {
-		errList["Other_error"] = "Please try again later"
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"status": http.StatusInternalServerError,
-			"error":  errList,
-		})
-		return
-	}
-	comment := models.Comment{}
-	like := models.Like{}
-
-	// Also delete the likes and the comments that this post have:
-	_, err = comment.DeletePostComments(server.DB, pid)
-	if err != nil {
-		errList["Other_error"] = "Please try again later"
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"status": http.StatusInternalServerError,
-			"error":  errList,
-		})
-		return
-	}
-	_, err = like.DeletePostLikes(server.DB, pid)
+	_, err = integration.DeleteAIntegration(server.DB)
 	if err != nil {
 		errList["Other_error"] = "Please try again later"
 		c.JSON(http.StatusInternalServerError, gin.H{
@@ -202,6 +250,6 @@ func (server *Server) DeletePost(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{
 		"status":   http.StatusOK,
-		"response": "Post deleted",
+		"response": "Item deleted",
 	})
 }
