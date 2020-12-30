7f2059f26696d2670477c95f74e9a359613570c0
Qwelian D. Tanner <qweliantanner@gmail.com>
2020-12-30 12:04:46 -0500

    user tesets

--- tests/setup_test.go
+++ tests/setup_test.go
@@ -8,15 +8,13 @@ import (
 	"os"
 	"testing"
 
-	"github.com/victorsteven/forum/api/controllers"
-	"github.com/victorsteven/forum/api/models"
+	"github.com/tapfunds/tfapi/api/controllers"
+	"github.com/tapfunds/tfapi/api/models"
 )
 
 var server = controllers.Server{}
 var userInstance = models.User{}
-var postInstance = models.Post{}
-var likeInstance = models.Like{}
-var commentInstance = models.Comment{}
+var tokenInstance = models.PlaidIntegration{}
 
 func TestMain(m *testing.M) {
 	//Since we add our .env in .gitignore, Circle CI cannot see it, so see the else statement
@@ -36,7 +34,7 @@ func TestMain(m *testing.M) {
 //When using CircleCI
 func CIBuild() {
 	var err error
-	DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", "127.0.0.1", "5432", "steven", "forum_db_test", "password")
+	DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", "127.0.0.1", "5432", "qwelian", "forum_db_test", "password")
 	server.DB, err = gorm.Open("postgres", DBURL)
 	if err != nil {
 		fmt.Printf("Cannot connect to %s database\n", "postgres")
@@ -51,18 +49,8 @@ func Database() {
 	var err error
 
 	TestDbDriver := os.Getenv("TEST_DB_DRIVER")
-	if TestDbDriver == "mysql" {
-		DBURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("TEST_DB_USER"), os.Getenv("TEST_DB_PASSWORD"), os.Getenv("TEST_DB_HOST"), os.Getenv("TEST_DB_PORT"), os.Getenv("TEST_DB_NAME"))
-		server.DB, err = gorm.Open(TestDbDriver, DBURL)
-		if err != nil {
-			fmt.Printf("Cannot connect to %s database\n", TestDbDriver)
-			log.Fatal("This is the error:", err)
-		} else {
-			fmt.Printf("We are connected to the %s database\n", TestDbDriver)
-		}
-	}
 	if TestDbDriver == "postgres" {
-		DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", os.Getenv("TEST_DB_HOST"), os.Getenv("TEST_DB_PORT"), os.Getenv("TEST_DB_USER"), os.Getenv("TEST_DB_NAME"), os.Getenv("TEST_DB_PASSWORD"))
+		DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s", os.Getenv("TEST_DB_HOST"), os.Getenv("TEST_DB_PORT"), os.Getenv("TEST_DB_USER"), os.Getenv("TEST_DB_NAME"), os.Getenv("TEST_DB_PASSWORD"))
 		server.DB, err = gorm.Open(TestDbDriver, DBURL)
 		if err != nil {
 			fmt.Printf("Cannot connect to %s database\n", TestDbDriver)
@@ -89,8 +77,8 @@ func refreshUserTable() error {
 func seedOneUser() (models.User, error) {
 
 	user := models.User{
-		Username: "Pet",
-		Email:    "pet@example.com",
+		Username: "Dee",
+		Email:    "dee@example.com",
 		Password: "password",
 	}
 
@@ -109,13 +97,13 @@ func seedUsers() ([]models.User, error) {
 	}
 	users := []models.User{
 		models.User{
-			Username: "Steven",
-			Email:    "steven@example.com",
+			Username: "Qwelian",
+			Email:    "qwelian@example.com",
 			Password: "password",
 		},
 		models.User{
-			Username: "Kenny",
-			Email:    "kenny@example.com",
+			Username: "Nala",
+			Email:    "nala@example.com",
 			Password: "password",
 		},
 	}
@@ -131,11 +119,11 @@ func seedUsers() ([]models.User, error) {
 
 func refreshUserAndPostTable() error {
 
-	err := server.DB.DropTableIfExists(&models.User{}, &models.Post{}).Error
+	err := server.DB.DropTableIfExists(&models.User{}, &models.PlaidIntegration{}).Error
 	if err != nil {
 		return err
 	}
-	err = server.DB.AutoMigrate(&models.User{}, &models.Post{}).Error
+	err = server.DB.AutoMigrate(&models.User{}, &models.PlaidIntegration{}).Error
 	if err != nil {
 		return err
 	}
@@ -143,56 +131,59 @@ func refreshUserAndPostTable() error {
 	return nil
 }
 
-func seedOneUserAndOnePost() (models.User, models.Post, error) {
+func seedOneUserAndOnePost() (models.User, models.PlaidIntegration, error) {
 
 	user := models.User{
-		Username: "Sam",
-		Email:    "sam@example.com",
+		Username: "HannahArendt",
+		Email:    "hannaharendt@example.com",
 		Password: "password",
 	}
 	err := server.DB.Model(&models.User{}).Create(&user).Error
 	if err != nil {
-		return models.User{}, models.Post{}, err
+		return models.User{}, models.PlaidIntegration{}, err
 	}
-	post := models.Post{
-		Title:    "This is the title sam",
-		Content:  "This is the content sam",
-		AuthorID: user.ID,
+	token := models.PlaidIntegration{
+		ItemID:    "This is the item",
+		AccessToken:  "This is the access token",
+		PaymentID:  "This is the payment id",
+		UserID: user.ID,
 	}
-	err = server.DB.Model(&models.Post{}).Create(&post).Error
+	err = server.DB.Model(&models.PlaidIntegration{}).Create(&token).Error
 	if err != nil {
-		return models.User{}, models.Post{}, err
+		return models.User{}, models.PlaidIntegration{}, err
 	}
-	return user, post, nil
+	return user, token, nil
 }
 
-func seedUsersAndPosts() ([]models.User, []models.Post, error) {
+func seedUsersAndPosts() ([]models.User, []models.PlaidIntegration, error) {
 
 	var err error
 
 	if err != nil {
-		return []models.User{}, []models.Post{}, err
+		return []models.User{}, []models.PlaidIntegration{}, err
 	}
 	var users = []models.User{
 		models.User{
-			Username: "Steven",
-			Email:    "steven@example.com",
+			Username: "Qwelian",
+			Email:    "qwelian@example.com",
 			Password: "password",
 		},
 		models.User{
-			Username: "Magu",
-			Email:    "magu@example.com",
+			Username: "Michele",
+			Email:    "mfoucault@example.com",
 			Password: "password",
 		},
 	}
-	var posts = []models.Post{
-		models.Post{
-			Title:   "Title 1",
-			Content: "Hello world 1",
+	var tokens = []models.PlaidIntegration{
+		models.PlaidIntegration{
+			ItemID:   "ItemID 1",
+			AccessToken: "AccessToken 1",
+			PaymentID: "PaymentID 1",
 		},
-		models.Post{
-			Title:   "Title 2",
-			Content: "Hello world 2",
+		models.PlaidIntegration{
+			ItemID:   "ItemID 2",
+			AccessToken: "AccessToken 2",
+			PaymentID: "PaymentID 2",
 		},
 	}
 
@@ -201,134 +192,80 @@ func seedUsersAndPosts() ([]models.User, []models.Post, error) {
 		if err != nil {
 			log.Fatalf("cannot seed users table: %v", err)
 		}
-		posts[i].AuthorID = users[i].ID
+		tokens[i].UserID = users[i].ID
 
-		err = server.DB.Model(&models.Post{}).Create(&posts[i]).Error
+		err = server.DB.Model(&models.PlaidIntegration{}).Create(&tokens[i]).Error
 		if err != nil {
-			log.Fatalf("cannot seed posts table: %v", err)
+			log.Fatalf("cannot seed tokens table: %v", err)
 		}
 	}
-	return users, posts, nil
+	return users, tokens, nil
 }
 
 func refreshUserPostAndLikeTable() error {
-	err := server.DB.DropTableIfExists(&models.User{}, &models.Post{}, &models.Like{}).Error
+	err := server.DB.DropTableIfExists(&models.User{}, &models.PlaidIntegration{}).Error
 	if err != nil {
 		return err
 	}
-	err = server.DB.AutoMigrate(&models.User{}, &models.Post{}, &models.Like{}).Error
+	err = server.DB.AutoMigrate(&models.User{}, &models.PlaidIntegration{}).Error
 	if err != nil {
 		return err
 	}
-	log.Printf("Successfully refreshed user, post and like tables")
+	log.Printf("Successfully refreshed user, token and like tables")
 	return nil
-}
-
-func seedUsersPostsAndLikes() (models.Post, []models.User, []models.Like, error) {
-	// The idea here is: two users can like one post
-	var err error
-	var users = []models.User{
-		models.User{
-			Username: "Steven",
-			Email:    "steven@example.com",
-			Password: "password",
-		},
-		models.User{
-			Username: "Magu",
-			Email:    "magu@example.com",
-			Password: "password",
-		},
-	}
-	post := models.Post{
-		Title:   "This is the title",
-		Content: "This is the content",
-	}
-	err = server.DB.Model(&models.Post{}).Create(&post).Error
-	if err != nil {
-		log.Fatalf("cannot seed post table: %v", err)
-	}
-	var likes = []models.Like{
-		models.Like{
-			UserID: 1,
-			PostID: post.ID,
-		},
-		models.Like{
-			UserID: 2,
-			PostID: post.ID,
-		},
-	}
-	for i, _ := range users {
-		err = server.DB.Model(&models.User{}).Create(&users[i]).Error
-		if err != nil {
-			log.Fatalf("cannot seed users table: %v", err)
-		}
-		err = server.DB.Model(&models.Like{}).Create(&likes[i]).Error
-		if err != nil {
-			log.Fatalf("cannot seed likes table: %v", err)
-		}
-	}
-	return post, users, likes, nil
 }
 
 func refreshUserPostAndCommentTable() error {
-	err := server.DB.DropTableIfExists(&models.User{}, &models.Post{}, &models.Comment{}).Error
+	err := server.DB.DropTableIfExists(&models.User{}, &models.PlaidIntegration{}).Error
 	if err != nil {
 		return err
 	}
-	err = server.DB.AutoMigrate(&models.User{}, &models.Post{}, &models.Comment{}).Error
+	err = server.DB.AutoMigrate(&models.User{}, &models.PlaidIntegration{}).Error
 	if err != nil {
 		return err
 	}
-	log.Printf("Successfully refreshed user, post and comment tables")
+	log.Printf("Successfully refreshed user, token and comment tables")
 	return nil
 }
 
-func seedUsersPostsAndComments() (models.Post, []models.User, []models.Comment, error) {
-	// The idea here is: two users can comment one post
+func seedUsersPostsAndComments() (models.User, []models.PlaidIntegration, error) {
+	// The idea here is: one user can have two tokens
 	var err error
-	var users = []models.User{
-		models.User{
-			Username: "Steven",
-			Email:    "steven@example.com",
-			Password: "password",
-		},
-		models.User{
-			Username: "Magu",
-			Email:    "magu@example.com",
-			Password: "password",
-		},
+	var user = models.User{
+		Username: "Adorno",
+		Email:    "tadorno@example.com",
+		Password: "password",
 	}
-	post := models.Post{
-		Title:   "This is the title",
-		Content: "This is the content",
-	}
-	err = server.DB.Model(&models.Post{}).Create(&post).Error
+
+	err = server.DB.Model(&models.User{}).Create(&user).Error
 	if err != nil {
-		log.Fatalf("cannot seed post table: %v", err)
+		log.Fatalf("cannot seed users table: %v", err)
 	}
-	var comments = []models.Comment{
-		models.Comment{
-			Body:   "user 1 made this comment",
-			UserID: 1,
-			PostID: post.ID,
+
+	var tokens = []models.PlaidIntegration{
+		models.PlaidIntegration{
+			
+			User: user,
+			UserID: user.ID,
+			ItemID:   "Adorno made this ItemID",
+			AccessToken:   "Adorno made this AccessToken",
+			PaymentID:   "Adorno made this PaymentID",
 		},
-		models.Comment{
-			Body:   "user 2 made this comment",
-			UserID: 2,
-			PostID: post.ID,
+		models.PlaidIntegration{
+			User:        user,
+			UserID:      user.ID,
+			ItemID:      "Adornomade this ItemID",
+			AccessToken: "Adorno made this AccessToken",
+			PaymentID:   "Adorno made this PaymentID",
 		},
 	}
-	for i, _ := range users {
-		err = server.DB.Model(&models.User{}).Create(&users[i]).Error
+	for i, _ := range tokens {
+		err = server.DB.Model(&models.PlaidIntegration{}).Create(&tokens[i]).Error
 		if err != nil {
-			log.Fatalf("cannot seed users table: %v", err)
-		}
-		err = server.DB.Model(&models.Like{}).Create(&comments[i]).Error
-		if err != nil {
-			log.Fatalf("cannot seed comments table: %v", err)
+			log.Fatalf("cannot seed tokens table: %v", err)
 		}
 	}
-	return post, users, comments, nil
+	return user, tokens, nil
 }
 
 func refreshUserAndResetPasswordTable() error {
@@ -349,10 +286,11 @@ func seedResetPassword() (models.ResetPassword, error) {
 
 	resetDetails := models.ResetPassword{
 		Token: "awesometoken",
-		Email: "pet@example.com",
+		Email: "dee@example.com",
 	}
 	err := server.DB.Model(&models.ResetPassword{}).Create(&resetDetails).Error
 	if err != nil {
 		return models.ResetPassword{}, err
 	}
 	return resetDetails, nil
+}
