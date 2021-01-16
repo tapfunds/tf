dadc396081502655b916b707f0a1b69f716a6aec
Qwelian D. Tanner <qweliantanner@gmail.com>
2021-01-16 11:32:35 -0500

    edit kubernetes service deployment

--- build-dev.sh
+++ build-dev.sh
@@ -3,4 +3,3 @@ docker-compose -f docker-compose.dev.yml up -d --build
 docker tag tfapi_go qweliant/tfapi_go:latest
 docker push qweliant/tfapi_go
 kubectl delete service,deployment tfapi
-kubectl apply -f kubes.yaml
