d618c97773d319a96fdbb5db82635d7d653f97ac
Qwelian D. Tanner <qweliantanner@gmail.com>
2021-02-16 13:00:55 -0500

    seperate services

--- plaid/api/controllers/routes.go
+++ plaid/api/controllers/routes.go
@@ -12,14 +12,12 @@ func (s *Server) initializeRoutes() {
 		v1.GET("/status", s.Status)
 
 		//Plaid routes
-		v1.POST("/create_link_token", middlewares.TokenAuthMiddleware(), s.createLinkToken)
-		v1.POST("/set_access_token", middlewares.TokenAuthMiddleware(), s.getAccessToken)
-		v1.POST("/balance", middlewares.TokenAuthMiddleware(), s.balance)
-		v1.POST("/auth", middlewares.TokenAuthMiddleware(), s.authorize)
-		v1.POST("/accounts", middlewares.TokenAuthMiddleware(), s.accounts)
-		v1.POST("/plaid/item", middlewares.TokenAuthMiddleware(), s.item)
-		v1.POST("/identity", middlewares.TokenAuthMiddleware(), s.identity)
-		v1.POST("/transactions", middlewares.TokenAuthMiddleware(), s.transactions)
-		v1.POST("/transfer", middlewares.TokenAuthMiddleware(), s.transfer)
+		v1.POST("/balance", s.balance)
+		v1.POST("/auth", s.authorize)
+		v1.POST("/accounts", s.accounts)
+		v1.POST("/plaid/item", s.item)
+		v1.POST("/identity", s.identity)
+		v1.POST("/transactions", s.transactions)
+		v1.POST("/transfer", s.transfer)
 	}
 }
