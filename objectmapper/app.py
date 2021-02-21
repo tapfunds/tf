cc31659a78de5228795f519a961e0875459d251b
Qwelian D. Tanner <qweliantanner@gmail.com>
2021-02-21 00:37:40 -0500

    python switch

--- objectmapper/app.py
+++ objectmapper/app.py
@@ -1,15 +1,18 @@
 from typing import Optional
-
 from fastapi import FastAPI
 import requests
 
 app = FastAPI()
 
-
 @app.get("/")
 async def read_root():
+
+    # // not real 
     req = requests.post(url="http://localhost:8000/api/v1/identity", data={"access_token": "access-sandbox-1ebc4747-dde5-4ec0-b2ef-0c69983b9362"})
-    print(req.text)
-    return {"Ok, so like I finished, but.>>":"WEEL IT KEEL"}
+    
+    lenth = len(req.json()["identity"])
+    for i in range(lenth):
+        lenth
+    return {"WIIL IT KEEL": lenth}
 
 
