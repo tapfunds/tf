0bb4e3b8008d66ecc884084aa619e3babb6b432e
Qwelian D. Tanner <qweliantanner@gmail.com>
2021-03-09 17:46:57 -0500

    main user page almost done

--- tapfunds/src/Components/ProfilePage/BankTap.js
+++ tapfunds/src/Components/ProfilePage/BankTap.js
@@ -1,14 +1,21 @@
-import React from 'react';
-
-
+import React from "react";
 
 const BankTap = () => {
-
-    return(
+  return (
     <div>
-    still need a card design
+      <div>
+        still need a card design
+        <div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+          <div class="relative py-3 sm:max-w-xl sm:mx-auto">
+            <div class="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg  sm:rounded-3xl"></div>
+            <div class="relative   sm:p-20">
+              <div class="max-w-md mx-auto">Lets get physical</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    );
+  );
 };
 
 export default BankTap;
