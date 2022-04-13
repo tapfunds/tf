12aac00eeebf2627f34d699ad0e512fa266d1ea7
qweliant <dmtorcode@gmail.com>
2022-04-13 00:54:19 -0400

    home page base done

--- client/pages/home.tsx
+++ client/pages/home.tsx
@@ -59,9 +59,7 @@ const Home: NextPage = () => {
     <div>
       <header className="bg-white shadow">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-          <h1 className=" text-3xl font-bold text-gray-900">
-            Welcome, mister X
-          </h1>
+          <h1 className=" text-3xl font-bold text-gray-900">Hello, X!</h1>
         </div>
       </header>
       <div className="grid grid-cols-2 gap-2 bg-blue-50 ">
@@ -70,25 +68,12 @@ const Home: NextPage = () => {
             <BankCard name={bank.name} color={bank.color} />
           </div>
         ))}
-        <div className=" py-6 flex-1 flex-col justify-start sm:py-9">
-          <div
-            style={{ width: "35%" }}
-            className="relative sm:max-w-xl sm:mx-auto "
-          >
-            <div className="absolute inset-0 shadow-lg bg-white sm:rounded-3xl"></div>
-            <div className="relative sm:p-20 px-4 pb-4">
-              <div className="max-w-md mx-auto">
-                <a href="/accounts">
-                    <Image
-                      src="/logo.svg"
-                      alt="Vercel Logo"
-                      width={72}
-                      height={16}
-                    />
-                </a>
-              </div>
+        <div className="py-6 flex-1 flex-col justify-start sm:py-9 relative sm:max-w-xl sm:mx-auto ">
+          <a href="/accounts">
+            <div>
+              <Image src="/plus.svg" alt="Plus Sign" width={100} height={100} />
             </div>
-          </div>
+          </a>
         </div>
       </div>
     </div>
